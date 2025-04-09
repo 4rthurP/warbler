095481d4da529ed452f371b802fc03f3a3d9b055
4rthurP <arthurperez@hotmail.fr>
2025-04-10 00:12:38 +0200

    Feat: cleaned watcher class and made run saving optional via config

--- src/classes/watcher.py
+++ src/classes/watcher.py
@@ -11,41 +11,51 @@ from .. import engine, LOCAL_TZ
 from ..models import WatcherRun
 
 class Watcher:
-    name: str
     notifiers: List[Notifier] = []
-    config: Config
-    source: str
-
     entries: dict[str, List[Entry]]
 
     def __init__(
             self, 
             config: Config
     ):
-        self.config = config
-        self.name = config.get("name")
-        self.source = config.get("source")
+        self.config: Config = config
+        self.name: str = config.get("name")
+        self.source: str = config.get("source")
+        self.save_if_empty: bool = config.get("save_if_empty", True)
 
     def load(self):
+        """Load the watcher configuration"""
         notifiers = self.config.get('notifiers')
         for notifier in notifiers:
             self.add_notifier(notifier)
 
     def run(self):
+        """
+        Run the watcher. This method will be called by the main loop.
+        It will load the watcher, find the new entries, save them to the database
+        and send them to the notifiers.
+        The watcher will also save the run to the database.
+        """
+        # Load the watcher configuration
         self.load()
+
+        # Saves the watcher's infos in a ORM model
         run = WatcherRun(
             name = self.name,
             run_start = datetime.now(),
             run_status = 'running',
         )
 
-        start_date = self.find_latest_run_date()
-        self.entries = self.find_new_entries(start_date)
-        self.save_new_entries(run)
-        self.send_new_entries()
-        self.save_run(run)
+        start_date = self.find_latest_run_date() # Get the latest run date
+        self.entries = self.find_new_entries(start_date) # Find the new entries since this date
+        self.save_new_entries(run) # Save the new entries to the database
+        self.send_new_entries() # Send the new entries to the notifiers
+        self.save_run(run) # Save the watcher's run to the database
 
     def find_latest_run_date(self):
+        """Find the latest run date for this watcher"""
+
+        # Get the latest run from the database
         session = Session(engine)
         latest_run: WatcherRun = (
             session
@@ -58,19 +68,15 @@ class Watcher:
 
         if latest_run:
             return latest_run.run_start.astimezone(LOCAL_TZ)
+        
+        # If there is no run, return the epoch
         return datetime(1970, 1, 1, tzinfo=LOCAL_TZ)
     
     def find_new_entries(self, start_date: datetime) -> List:
         pass
 
-    def parse_entries_from_logs(self):
-        for log in self.logs:
-            service = self.find_service(log.source)
-            if service:
-                entries = service.parse_entries(log)
-                self.entries.extend(entries)
-
     def save_new_entries(self, run: WatcherRun):
+        """Save the new entries to the database"""
         session = Session(engine)
         for entry in self.entries:
             logging.info(f"Saving entry {entry} for run {run.id}")
@@ -79,24 +85,29 @@ class Watcher:
         session.close()
 
     def send_new_entries(self):
+        """Send the new entries to all notifiers"""
         for notifier in self.notifiers:
             notifier.send(self.entries)
 
     def save_run(self, run: WatcherRun):
+        """Save the run to the database"""
+
+        # Check if there are no new entries and save the run if configured to do so
+        if len(self.entries) == 0 and not self.save_if_empty:
+            logging.info(f"Watcher {self.name} has no new entries and will not save the run")
+            return
+        
+        # Update the run's infos
         run.run_end = datetime.now()
         run.run_status = 'complete'
+
+        # Save the run to the database
         session = Session(engine)
         session.add(run)
         session.commit()
         session.close()
 
     def add_notifier(self, notifier: Notifier):
+        """ Add a notifier to the watcher """
         self.notifiers.append(notifier)
-
-    def find_service(self, source: str):
-        if source in self.services:
-            return self.services[source]
-        
-        logging.error(f'No service found for source {source}')
-        return None
     
