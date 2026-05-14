f5a94977b9f3e59585941c83d88a51eee08403d4
Arthur <arthurp@tuta.com>
2026-05-14 17:27:38 +0200

    chore: update packages

--- src/warbler/run-script.sh
+++ src/warbler/run-script.sh
@@ -9,19 +9,22 @@ fi
 
 SCRIPT="$1"
 
+APP_PATH="${APP_PATH:-/var/app}"
+cd "$APP_PATH"
+
 # Set up the virtual environment
-export PATH="/var/app/.venv/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin"
-export PYTHONPATH="/var/app/.venv/bin"
-source /var/app/.venv/bin/activate
+export PATH="$APP_PATH/.venv/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin"
+export PYTHONPATH="$APP_PATH/.venv/bin"
+source "$APP_PATH/.venv/bin/activate"
 
 # Load .env safely
-if [ -f "/var/app/.env" ]; then
+if [ -f "$APP_PATH/.env" ]; then
   set -a  # automatically export all variables
-  source /var/app/.env
+  source "$APP_PATH/.env"
   set +a
 fi
 
 # Run the script
 cd /var
 LOG_LOCATION="${WARBLER_LOG_PATH:-/var}"
-/var/app/.venv/bin/python3 -u -m app.$SCRIPT >> "$LOG_LOCATION"/runner.log 2>&1
+.venv/bin/python3 -u -m app.$SCRIPT >> "$LOG_LOCATION"/runner.log 2>&1
