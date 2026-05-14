7fa94a812227fd70a45b10a83d884400d56af2e9
Arthur <arthurp@tuta.com>
2026-05-14 16:47:58 +0200

    feat: add sqlite support

--- src/warbler/__init__.py
+++ src/warbler/__init__.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from zoneinfo import ZoneInfo
 
 from dotenv import load_dotenv
@@ -8,24 +9,28 @@ from .models import Base
 
 load_dotenv()
 
-engine = create_engine(
-    f"mysql+pymysql://{os.getenv('WARBLER_DATABASE_USER')}:{os.getenv('WARBLER_DATABASE_PASSWORD')}@{os.getenv('WARBLER_DATABASE_HOST')}:{os.getenv('WARBLER_DATABASE_PORT')}/{os.getenv('WARBLER_DATABASE_NAME')}"
-)
+sqlite_path = os.getenv("WARBLER_SQLITE_PATH")
+if sqlite_path:
+    engine = create_engine(f"sqlite:///{sqlite_path}/warbler.sqlite")
+else:
+    engine = create_engine(
+        f"mysql+pymysql://{os.getenv('WARBLER_DATABASE_USER')}:{os.getenv('WARBLER_DATABASE_PASSWORD')}@{os.getenv('WARBLER_DATABASE_HOST')}:{os.getenv('WARBLER_DATABASE_PORT')}/{os.getenv('WARBLER_DATABASE_NAME')}"
+    )
 
 LOCAL_TZ = ZoneInfo(os.getenv("LOCAL_TZ", "Europe/Paris"))
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-LOCK_FILE = os.path.join(APP_ROOT, "initialized.lock")
+APP_ROOT = Path(__file__).resolve().parent
+LOCK_FILE = APP_ROOT / "initialized.lock"
 
 
 def initialize_database():
-    if os.path.exists(LOCK_FILE):
+    if LOCK_FILE.exists():
         return
 
     # Create all tables from SQLAlchemy models
     Base.metadata.create_all(engine)
 
-    with open(LOCK_FILE, "w") as f:
+    with LOCK_FILE.open("w") as f:
         f.write("initialized")
 
 
